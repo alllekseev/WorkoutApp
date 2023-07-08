69cbccd335d519e720f3efcf269b16c1e19304cf
Alekseev Oleg <01.wftp@gmail.com>
2023-07-08 08:47:55 +0300

    Make TabBar

---
+++ WorkoutApp/Application/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  WorkoutApp
+//
+//  Created by Олег Алексеев on 08.07.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
