7bbda1b1e74809f2e72b74e427d54b6443479ecc
Alekseev Oleg <01.wftp@gmail.com>
2023-07-11 10:46:12 +0300

    Setup TimerView

---
+++ WorkoutApp/Controllers/Session/Views/Timer/ProgressView.swift
@@ -0,0 +1,16 @@
+//
+//  ProgressView.swift
+//  WorkoutApp
+//
+//  Created by Олег Алексеев on 11.07.2023.
+//
+
+import UIKit
+
+extension TimerView {
+    final class ProgressView: UIView {
+        func drawProgress(with percent: CGFloat) {
+            
+        }
+    }
+}
